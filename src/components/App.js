2e44d32a3d769bd16c7b82b666fdd5235ec09bcd
John Shearer <john@ultranaut.com>
2020-08-02 21:01:54 -0400

    Move Display component into App

---
+++ src/components/App.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import telegraph from '../images/L-Telegraph1_mod.png';
+// import '../index.css';
+
+class App extends React.Component {
+  state = {
+    input: '',
+    output: '',
+    dotDuration: 200, // 6wpm
+    hotKey: 18, // alt/option key
+    signalOn: false,
+    signalStart: 0,
+    signalEnd: 0,
+  };
+
+  componentDidMount = () => {
+    this.codeMap = this.props.codeMap;
+  };
+
+  updateInput = (inputValue) => {
+    this.setState(
+      {
+        input: inputValue,
+      },
+      this.decodeInput
+    );
+  };
+
+  clearInput = () => {
+    this.setState(
+      {
+        input: '',
+      },
+      this.decodeInput
+    );
+  };
+
+  handleSignalStart = (e) => {
+    // prevent keydown and touchstart events from repeatedly
+    // triggering this
+    if (
+      e.keyCode === this.state.hotKey ||
+      (e.type === 'touchstart' && !this.state.signalOn)
+    ) {
+      // let me handle this, thank you
+      e.preventDefault();
+
+      var now = window.performance.now();
+
+      if (this.state.signalEnd > 0) {
+        var pause = now - this.state.signalEnd;
+      }
+
+      // if really long pause, start a new word
+      if (pause > 7 * this.state.dotDuration) {
+        this.setState({
+          input: this.state.input + ' / ',
+        });
+      } else if (pause > 3 * this.state.dotDuration) {
+        this.setState({
+          input: this.state.input + ' ',
+        });
+      }
+      this.setState({ signalEnd: 0 });
+
+      this.setState({ signalOn: true, signalStart: now });
+    }
+  };
+
+  handleSignalEnd = () => {
+    if (this.state.signalOn === true) {
+      var now = window.performance.now();
+      var duration = now - this.state.signalStart;
+      var input = '';
+      if (duration < this.state.dotDuration) {
+        input = '.';
+      } else {
+        input = '-';
+      }
+      this.setState({
+        signalOn: false,
+        signalStart: 0,
+        signalEnd: now,
+        input: this.state.input + input,
+      });
+      this.decodeInput();
+    }
+  };
+
+  decodeInput = () => {
+    var tokens = this.state.input.split(' ');
+    console.log(tokens);
+    var decoded = '';
+    var char;
+    for (var idx in tokens) {
+      var token = tokens[idx];
+      if (token === '') {
+        continue;
+      }
+      if (token === '/') {
+        char = ' ';
+      } else {
+        char = this.codeMap[token];
+      }
+      decoded += typeof char !== 'undefined' ? char : '';
+    }
+    this.setState({ output: decoded });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Fig. 6.</h1>
+          <img src={telegraph} alt="Fig. 6." />
+        </header>
+        {/* <Tapper */}
+        {/*   handleSignalStart={this.props.handleSignalStart} */}
+        {/*   handleSignalEnd={this.props.handleSignalEnd} */}
+        {/* /> */}
+        {/* <Output output={this.props.output} clearInput={this.props.clearInput} /> */}
+      </div>
+    );
+  }
+}
+
+// var App = React.createClass({
+
+//   codeMap: null,
+
+//   render: function () {
+//     return React.createElement(Display, {
+//       output: this.state.output,
+//       input: this.state.input,
+//       updateInput: this.updateInput,
+//       clearInput: this.clearInput,
+//       handleSignalStart: this.handleSignalStart,
+//       handleSignalEnd: this.handleSignalEnd,
+//     });
+//   },
+// });
+
+export default App;
