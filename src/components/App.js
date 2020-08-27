bb4af6b36ddc3fb858c06f3e03a9e00403a2aeb4
John Shearer <john@ultranaut.com>
2020-08-27 14:32:09 -0400

    Move Tapper state and functionality into component

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,6 @@ class App extends React.Component {
   state = {
     input: '',
     output: '',
-    dotDuration: 200, // 6wpm
-    hotKey: 18, // alt/option key
-    signalOn: false,
-    signalStart: 0,
-    signalEnd: 0,
   };
 
   codeMap = codeMap;
@@ -35,63 +30,8 @@ class App extends React.Component {
     );
   };
 
-  handleSignalStart = (e) => {
-    let pause = 0;
-
-    // prevent keydown and touchstart events from repeatedly
-    // triggering this
-    if (
-      e.keyCode === this.state.hotKey ||
-      (e.type === 'touchstart' && !this.state.signalOn)
-    ) {
-      // let me handle this, thank you
-      e.preventDefault();
-
-      const now = window.performance.now();
-
-      if (this.state.signalEnd > 0) {
-        pause = now - this.state.signalEnd;
-      }
-
-      // if really long pause, start a new word
-      if (pause > 7 * this.state.dotDuration) {
-        this.setState({
-          input: this.state.input + ' / ',
-        });
-      } else if (pause > 3 * this.state.dotDuration) {
-        this.setState({
-          input: this.state.input + ' ',
-        });
-      }
-      this.setState({ signalEnd: 0 });
-
-      this.setState({ signalOn: true, signalStart: now });
-    }
-  };
-
-  handleSignalEnd = () => {
-    if (this.state.signalOn === true) {
-      const now = window.performance.now();
-      const duration = now - this.state.signalStart;
-      let input = '';
-      if (duration < this.state.dotDuration) {
-        input = '.';
-      } else {
-        input = '-';
-      }
-      this.setState({
-        signalOn: false,
-        signalStart: 0,
-        signalEnd: now,
-        input: this.state.input + input,
-      });
-      this.decodeInput();
-    }
-  };
-
   decodeInput = () => {
     const tokens = this.state.input.split(' ');
-    console.log(tokens);
     let decoded = '';
     let char;
     for (const idx in tokens) {
@@ -116,10 +56,7 @@ class App extends React.Component {
           <h1>Fig. 6.</h1>
           <img src={telegraph} alt="Fig. 6." />
         </header>
-        <Tapper
-          handleSignalStart={this.handleSignalStart}
-          handleSignalEnd={this.handleSignalEnd}
-        />
+        <Tapper updateAppInput={this.updateInput} />
         <Output output={this.state.output} clearInput={this.clearInput} />
       </div>
     );
--- src/components/Tapper.js
+++ src/components/Tapper.js
@@ -1,17 +1,96 @@
 import React from 'react';
 
 class Tapper extends React.Component {
-  state = { display: '' };
+  config = {
+    hotKey: 18, // alt/option key
+    dotDuration: 200, // 6wpm
+  };
+
+  state = {
+    input: '',
+    signalOn: false,
+    signalStart: 0,
+    signalEnd: 0,
+  };
+
+  handleSignalStart = (e) => {
+    let pause = 0;
+
+    // prevent keydown and touchstart events from repeatedly
+    // triggering this
+    if (
+      e.keyCode === this.config.hotKey ||
+      (e.type === 'touchstart' && !this.state.signalOn)
+    ) {
+      // let me handle this, thank you
+      e.preventDefault();
+
+      // get the time our signal started
+      const now = window.performance.now();
+
+      // figure out how long ago the previous signal ended
+      if (this.state.signalEnd > 0) {
+        pause = now - this.state.signalEnd;
+      }
+
+      // start a new signal
+      this.setState({
+        signalOn: true,
+        signalStart: now,
+        signalEnd: 0,
+      });
+
+      // if really long pause, indicate a new word...
+      if (pause > 7 * this.config.dotDuration) {
+        this.setState({
+          input: this.state.input + ' / ',
+        });
+      }
+      // ...otherwise it's a new character
+      else if (pause > 3 * this.config.dotDuration) {
+        this.setState({
+          input: this.state.input + ' ',
+        });
+      }
+      // default: it's a dit or a dah
+    }
+  };
+
+  handleSignalEnd = () => {
+    if (this.state.signalOn === true) {
+      const now = window.performance.now();
+      const signalDuration = now - this.state.signalStart;
+      let input = '';
+
+      // dit or dah?
+      if (signalDuration < this.config.dotDuration) {
+        input = '.';
+      } else {
+        input = '-';
+      }
+
+      // clear the active signal
+      this.setState({
+        signalOn: false,
+        signalStart: 0,
+        signalEnd: now,
+        input: this.state.input + input,
+      });
+
+      console.log('input:', this.state.input);
+      this.props.updateAppInput(this.state.input);
+    }
+  };
 
   componentDidMount = () => {
     // keyboard events
-    window.addEventListener('keydown', this.props.handleSignalStart, false);
-    window.addEventListener('keyup', this.props.handleSignalEnd, false);
+    window.addEventListener('keydown', this.handleSignalStart, false);
+    window.addEventListener('keyup', this.handleSignalEnd, false);
 
     // touch events
     const pad = document.getElementById('tap-pad');
-    pad.addEventListener('touchstart', this.props.handleSignalStart, false);
-    pad.addEventListener('touchend', this.props.handleSignalEnd, false);
+    pad.addEventListener('touchstart', this.handleSignalStart, false);
+    pad.addEventListener('touchend', this.handleSignalEnd, false);
   };
 
   render() {
