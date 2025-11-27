f6a39acdc1365b1594780348580af1f929aa7747
John Shearer <440100+ultranaut@users.noreply.github.com>
2025-11-27 16:54:20 -0500

    Use the React 18 createRoot API correctly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './css/index.css';
 import './css/main.css';
 import App from './components/App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
